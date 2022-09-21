3cdf419d940efb3f26bcd6b2d2d35b66ca111432
yldzgzm <yldzgzm1211@gmail.com>
2022-09-21 23:55:26 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to MineSweeper!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter the number of rows: ");
+        int r = scan.nextInt();
+        System.out.print("Please enter the number of columns: ");
+        int c = scan.nextInt();
+
+        MineSweeper mine = new MineSweeper(r, c);
+
+        mine.printArea();
+
+        do {
+            System.out.print("Enter the row: ");
+            r = scan.nextInt();
+            System.out.print("Enter the column: ");
+            c = scan.nextInt();
+            mine.sweep(r, c);
+        }
+
+        while (!mine.isGameOver);
+
+    }
+}
